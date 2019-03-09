f74e361cdca37d47a913787ba0d38cacc989a43e
Kyle Treptow <treptow@apollointeractive.com>
2019-03-08 17:54:51 -0800

    colors module for console

--- index.js
+++ index.js
@@ -1,30 +1,20 @@
 // Modules
 var today = require("./modules/today.js");
 var server = require("./modules/server.js");
-
-// Colors
-var
-dim = '\x1b[2m%s\x1b[0m',
-black = '\x1b[30m%s\x1b[0m',
-red = '\x1b[31m%s\x1b[0m',
-green = '\x1b[32m%s\x1b[0m',
-yellow = '\x1b[33m%s\x1b[0m',
-blue = '\x1b[34m%s\x1b[0m',
-magenta = '\x1b[35m%s\x1b[0m',
-cyan = '\x1b[36m%s\x1b[0m',
-white = '\x1b[37m%s\x1b[0m';
+var colors = require("./modules/colors.js");
 
 // Log
-console.log(dim, '///////////////////');
-console.log(cyan,'Sample Color Text');
+console.log(colors['dim'], '///////////////////');
+
 console.log(
-  'Logging Test #1',
+  'Node Test Server | ',
   today.getToday(),
   today.getTime('pst')
 );
-console.log(dim, '///////////////////');
+
+console.log(colors['dim'], '///////////////////');
 
 // Serve
 var port = 8080;
-console.log(green, 'Running server on port '+port)
+console.log(colors['green'], 'Running server on port '+port)
 server.runServer(port);
